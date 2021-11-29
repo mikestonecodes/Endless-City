9341b3b374dfb8ac35ab49d43bf6f52d002cdfd6
mikestonecodes <michaeljacobstone@gmail.com>
2021-11-29 16:57:02 +0100

    cleanup , add debug text, double click to chng

--- js/main.js
+++ js/main.js
@@ -20,6 +20,7 @@ var camera,
     loader = new THREE.GLTFLoader(manager),
     gun = GUN('https://stonegun.herokuapp.com/gun');
 
+
 var clusterNames = [
     'factory',
     'house2',
@@ -38,205 +39,197 @@ var clusterNames = [
     'park',
     'supermarket',
 ]
-var tiles = [];
-var startingPos =  {x:0,y:0};
 
+var startingPos = { x: 0, y: 0 };
+
+var _localTiles = {};
+
+let lookingAtTile = { x: 0, y: 0 }
+
+let pointingAtTile = {x: 0,y: 0}
+
+let globalPosition = { x: 0, y: 0 }
+
+let relOffset = { x: 0, z: 0 }
+
+let innerTileOffset = { x: 5, y: 5 }
+
+let mouseOnGround = {x:0,z:0}
+
+let listeningFor = [];
 
 // GUN STUFF 
-var gunTiles = gun.get('tiles');
+var gunTiles = gun.get('tiles')
 
-function lookedAtNewTile(pos){
-    if(this.pTile && Math.round(this.pTile.x)== Math.round(pos.x) && Math.round(this.pTile.y) == Math.round(pos.y)){
+function lookedAtNewTile(pos) {
+    if (this.pTile && Math.round(this.pTile.x) == Math.round(pos.x) && Math.round(this.pTile.y) == Math.round(pos.y)) {
         return;
     }
-    //console.log("looked at new Tile!","global",globalPosition,"local",lookingAtTile)
-    if(window.tileView){ tileView(globalPosition.x, globalPosition.y) } // Mark's function, comment this on/off to enable his code.
-    loadTilesRadius(globalPosition.x, globalPosition.y, 2);
-    
-    history.pushState(null,null,'#'+parseInt(globalPosition.x)+","+parseInt(globalPosition.y));
 
-    // POLLING FOR TILES?
-    //gunTiles.get(x).get(y).once(tile=>{}) ?
-    
+    loadTilesRadius(globalPosition.x, globalPosition.y, 2);
+
+    history.pushState(null, null, '#' + parseInt(globalPosition.x) + "," + parseInt(globalPosition.y));
+
     this.pTile = pos;
 }
 
-function setTile(x,y,type){
-    console.log("set", x+','+y, type);
-    gunTiles.get(x+','+y).put(type);
-    tileView(x,y);
+function setTile(x, y, type) {
+    gunTiles.get(x + ',' + y).put(type);
+    tileView(x, y);
 }
 
-function loadTilesRadius(x,y,radius){
+function loadTilesRadius(x, y, radius) {
+    x = Math.round(x), y = Math.round(y);
     tileView(x, y);
     x = Math.round(x);
     y = Math.round(y);
 
-    var i = 0; while(i++ < radius){ 
-        tileView(x+i, y);
-        tileView(x-i, y);
+    var i = 0; while (i++ < radius) {
+        tileView(x + i, y);
+        tileView(x - i, y);
 
-        tileView(x, y+i);
-        tileView(x, y-i);
+        tileView(x, y + i);
+        tileView(x, y - i);
 
-        tileView(x+i, y+i);
-        tileView(x-i, y-i);
+        tileView(x + i, y + i);
+        tileView(x - i, y - i);
 
-        tileView(x-i, y+i);
-        tileView(x+i, y-i);
+        tileView(x - i, y + i);
+        tileView(x + i, y - i);
     }
 }
 
-function tileView(x,y){
-    x = Math.round(x), y = Math.round(y);
-    //console.log("try to load", x,y);
-    var meta = gunTiles.get(x+','+y);
-    if(meta.watch){ return }
-    meta.watch = true;
-    meta.x = meta.x;
-    meta.y = meta.y;
-    meta.on(function(data){
-        console.log("see", x,y, data);
-        meta.type = data
-        if(!meta.tile){ return }
-      //  meta.tile.render();
+//ask and recieve updates from network
+function tileView(x, y) {
+    if(listeningFor.includes(x+","+y))return;
+    listeningFor.push(x+","+y);
+
+    gunTiles.get(x + ',' + y).on((data) => {
+        console.log("recieved",x,y,data);
+        console.log("listening To ", listeningFor)
     });
 }
 
-function getTile(x,y){
-    //if(!tiles[x])return null;
-    //return tiles[x][y];
-    var meta = gunTiles.get(x+','+y);
-    return meta;
+// this is getting called 60fpsx64 times a second! gun.get can work here?
+function getTile(x, y) {
+    if (!gun._.graph.tiles) return;
+    return gun._.graph.tiles[x + "," + y];
 }
 
-
-
-var _localTiles = [];
-
-var lclpos= [0,0]
-
-let lookingAtTile = {x:0,y:0}
-
-let globalPosition = {x:0,y:0}
-
-let relOffset = {x:0,z:0}
-
-let innerTileOffset = {x:5,y:5};
-
-let localPos = {x:0,y:0};
-
-function getCoordsFromHash(){
-    if(location.hash){
+window.addEventListener('hashchange', getCoordsFromHash, false);
+function getCoordsFromHash() {
+    if (location.hash) {
         let pos = location.hash.split("#")[1].split(",");
-        goToCoord({x:parseInt(pos[0]),y:parseInt(pos[1])});
+        goToCoord({ x: parseInt(pos[0]), y: parseInt(pos[1]) });
     }
 }
 
-function goToCoord({x,y}){
-    startingPos=  {x,y}
-    relOffset = {x:0,z:0};
+function goToCoord({ x, y }) {
+    startingPos = { x, y }
+    relOffset = { x: 0, z: 0 };
 
-    if(controls)controls.target.x = 0
-    if(camera)camera.position.x = 41.56269377774534
-    if(controls)controls.target.z = 0
-    if(camera)camera.position.z = 41.562693777745345
+    if (controls) controls.target.x = 0
+    if (camera) camera.position.x = 41.56269377774534
+    if (controls) controls.target.z = 0
+    if (camera) camera.position.z = 41.562693777745345
 }
 
 getCoordsFromHash();
 
-window.addEventListener('hashchange', getCoordsFromHash, false);
+function mktxtspr(message, fontsize,color) {
+    var ctx, texture, sprite, spriteMaterial,
+    canvas = document.createElement('canvas');
+    ctx = canvas.getContext('2d');
+    ctx.font = fontsize + "px Arial";
 
-loadTilesRadius(lookingAtTile.x, lookingAtTile.y, 1);
+    // setting canvas width/height before ctx draw, else canvas is empty
+    canvas.width = ctx.measureText(message).width;
+    canvas.height = fontsize * 2; // fontsize * 1.5
+
+    // after setting the canvas width/height we have to re-set font to apply!?! looks like ctx reset
+    ctx.font = fontsize + "px Arial";
+    ctx.fillStyle = color;
+    ctx.fillText(message, 0, fontsize);
+
+    texture = new THREE.Texture(canvas);
+    texture.minFilter = THREE.LinearFilter; // NearestFilter;
+    texture.needsUpdate = true;
+
+    spriteMaterial = new THREE.SpriteMaterial({ map: texture });
+    sprite = new THREE.Sprite(spriteMaterial);
+    return sprite;
+}
 
 var loaded = {};
-var thats = new Map;
 
-class Tile {
-     constructor(x,z,cluster){   
-         this.x = x;
-         this.z = z;
+
+class Tile{
+    constructor(localx, localz) {
+        this.localx = localx;
+        this.localz = localz;
+        this.currentTextColor = "rgb(255,0,0)"
+    }
+    getGlobalTilePosition() {
+        let rendoffx = (this.localx - lookingAtTile.x);
+        let rendoffy = (this.localz - lookingAtTile.y);
+        return { x: globalPosition.x + rendoffx, y: globalPosition.y + rendoffy }
+    }
+    updateText(text,textcolor){
+        if(text == this.text && textcolor == this.textcolor)return;
+        if(this.spi)scene.remove(this.spi);
+        let spi = mktxtspr(text,600,textcolor);
+        spi.position.set((this.localx-5)*60,10,(this.localz-5)*60);
+        spi.scale.set(15,15,15)
+        scene.add(spi);
+        this.spi = spi;
+        this.text = text;
+        this.textcolor = textcolor;
+    }
+    loadTile(tileType) {
+       // if(!tileType)tileType="park";
+        if (tileType === this.tileType) { return }
+        if (this.gtlfScene) { scene.remove(this.gtlfScene) }
+        this.tileType = tileType;
+        if (tileType) {
+            var url = `js/clusters/${tileType}.glb`;
+            loader.load(url, 
+            (gltf) => {
+                loaded[url] = gltf;
+                if (this.gtlfScene) scene.remove(this.gtlfScene)
+                this.gtlfScene = gltf.scene;
+                this.gtlfScene.traverse(function (child) {
+
+                    if (child.isMesh) {
+                        child.receiveShadow = true
+                        child.castShadow = true
+                    }
+
+                    child.userData.tile = this;
+                })
+                this.gtlfScene.position.set((this.localx - 5) * 60, 0, (this.localz - 5) * 60)
+               scene.add(this.gtlfScene)
+            });
+        }
+    }
+   
+    render() {
+    
+        if(this.localx==pointingAtTile.x && this.localz==pointingAtTile.y){
+            this.currentTextColor="rgb(0,0,255)"
+        }else{
+            this.currentTextColor="rgb(255,0,0)"
+        }
        
-     }
-
-     loadCluster(cluster) {
-        /*if(!cluster){ cluster = 'park' }
-        if(! cluster&& this.gtlfScene) {
-            scene.remove(this.gtlfScene )
-        }
-        if(cluster == this.cluster)return;
-        var that = this;
-        this.cluster = cluster;*/
-
-       // if(!cluster){ cluster = 'park' }
-        if(cluster === this.cluster){ return }
-        if(this.gtlfScene){ scene.remove(this.gtlfScene)}
-        this.cluster = cluster;
-        var that = this;
-        thats.set(that.lot.x+',')
-
-        if(cluster){
-            
-        //console.log("load", cluster, this);
-        var url = `js/clusters/${cluster}.glb`, tmp;
-
-        /*if(tmp = loaded[url]){
-            load(tmp)
-        } else {
-            loader.load(url,load);
-        }*/
-        loader.load(url,load);
-
-        function load(gltf){
-            loaded[url] = gltf;
-           if(that.gtlfScene )scene.remove(that.gtlfScene )
-           that.gtlfScene = gltf.scene; 
-           that.gtlfScene.traverse(function (child) {
-
-                if (child.isMesh) { 
-                    child.receiveShadow = true
-                    child.castShadow = true
-                }
-              
-                child.userData.tile = that;
-            })
-           
-            that.gtlfScene.position.set((that.x-5) * 60, 0, (that.z-5) * 60)  
-            scene.add(that.gtlfScene )
-        }
-    }
-    }
-    getGlobalPosition(){
-        let rendoffx = (this.x-lookingAtTile.x ) % 4;
-        let rendoffy= (this.z-lookingAtTile.y)  % 4;
+        let globalTilePosition = this.getGlobalTilePosition();
+        var tileType = getTile(globalTilePosition.x, globalTilePosition.y);
         
-        if(rendoffx < 0){
-         rendoffx = rendoffx % -4
-        }
-        if(rendoffy < 0){
-         rendoffy = rendoffy % -4
-        }
-        return {x:globalPosition.x+rendoffx,y:globalPosition.y+rendoffy}
-    }
-    render(){
-       
-
-        this.lot = this.lot || this.getGlobalPosition();
-        var lot = this.lot;
-        var meta = getTile(lot.x, lot.y);
-        meta.tile = this;
-
-        this.loadCluster(meta.type);
-
+        this.updateText(globalTilePosition.x+","+globalTilePosition.y,this.currentTextColor);
+        this.loadTile(tileType);
     }
 }
-  // Load map
 
 initCity()
 animate()
-
-
-
 
 function initCity() {
     // Statistics settings
@@ -244,7 +237,6 @@ function initCity() {
 
     document.body.appendChild(stats.dom)
 
-   
     // Scene settings
     scene = new THREE.Scene()
     scene.background = new THREE.Color(0x000000)
@@ -284,32 +276,27 @@ function initCity() {
 
     //Events
     window.addEventListener('resize', onResize, false)
+    window.addEventListener('dblclick', onMouseDblclick, false)
     window.addEventListener('mousemove', onMouseMove, false)
-    window.addEventListener('mousedown', onMouseDown, false)
-    window.addEventListener('mouseup', onMouseUp, false)
     window.addEventListener('touchmove', onMouseMove, false)
-    window.addEventListener('touchstart', onMouseDown, false)
-    window.addEventListener('touchend', onMouseUp, false)
-
-  
+    //window.addEventListener('touchstart', onMouseDown, false)
+    //window.addEventListener('touchend', onMouseUp, false)
 
     //8x8 road grid
     loader.load(`js/clusters/road.glb`, (gltf) => {
-        gltf.scene.position.set(60, 0, 0)  
-        scene.add(gltf.scene)
+        gltf.scene.position.set(60, 0, 0)
+        this.road = gltf.scene;
+        scene.add(this.road);
     })
-    
+
     for (let z = 0; z < 8; z++) {
         for (let x = 0; x < 8; x++) {
-         
-            _localTiles.push(new Tile(x,z));
+            _localTiles[x+","+z]=new Tile(x, z);
         }
     }
-    
 
-   // if (screen.width > 768) {
-        loadCars({ x: 1, z: 0, cluster: 'cars' })
-    //}
+    loadCars({ x: 1, z: 0, cluster: 'cars' })
+    loadTilesRadius(globalPosition.x, globalPosition.y, 1);
 }
 
 function onResize() {
@@ -319,108 +306,92 @@ function onResize() {
 }
 
 function onMouseMove(event) {
-
     event.preventDefault()
     mouse.x = (event.clientX / window.innerWidth) * 2 - 1
     mouse.y = -(event.clientY / window.innerHeight) * 2 + 1
-
     this.lookedAtNewTile(lookingAtTile);
-    //gun.get(data.X).get(data.Y).once(plant => { mikeGameLogic.addPlant(plant).onTile(data.X, data.Y) }) }
-}
 
-function onMouseDown(event){
-    onMouseDown.when = +new Date;
-}
-function onMouseUp(event){
-  //  onMouseUp.when = +new Date;
-   // if(onMouseUp.when - onMouseDown.when < 99){
-        onTileClick(event);
-  //  }
-}
 
-function onTileClick(event) {
-    event.preventDefault()
     raycaster.setFromCamera(mouse, camera);
-    var intersects = raycaster.intersectObject(scene, true);
-    if (intersects.length > 0) {
-        var object = intersects[0].object;
-        var tile = object.userData.tile
-        if(tile){
-       
-            let tileGlobalPosition = tile.getGlobalPosition();
-
-            var i = ((onTileClick.i + 1) % clusterNames.length);
-            onTileClick.i = i;
-            var type = this.clusterNames[i]
-
-            //console.log("click", onTileClick.i);
-            setTile(tileGlobalPosition.x,tileGlobalPosition.y,type);
+    if (this.road) {
+        var intersects = raycaster.intersectObject(this.road,true);
+        if (intersects.length > 0) {
+            const posX = intersects[0].point.x;
+            const posZ = intersects[0].point.z;
+            mouseOnGround = {x:posX, z:posZ};
         }
-     //  object.material.color.set( Math.random() * 0xffffff );
     }
 }
-onTileClick.i = 0;
+
+
+
+function onMouseDblclick(event) {
+    let curtil = this._localTiles[pointingAtTile.x+","+pointingAtTile.y];
+    let tileToSet = curtil.getGlobalTilePosition();
+    setTile(tileToSet.x,tileToSet.y,clusterNames[Math.floor(Math.random()*16)]);
+}
+
 
 function animate() {
     requestAnimationFrame(animate)
     render()
 }
 
-function goTo(x,y){
-    this.globalPosition = {x,y}    
+function goTo(x, y) {
+    this.globalPosition = { x, y }
 }
 
 function render() {
-
-    previousLookingAtTile = lookingAtTile;
-
+ 
     stats.begin()
     controls.update()
-   
-    let rx = 1-((130 - (camera.position.x - 60 ) )/ 420 )
-    let rz = 1-((130 - (camera.position.z -60  ) )/ 420 ) 
-   
-    lookingAtTile = {x:(rx*8)  , y:(rz*8)};
-     
-  
-     globalPosition = {x:startingPos.x +relOffset.x + (lookingAtTile.x-innerTileOffset.x)  ,y:startingPos.y +relOffset.z + (lookingAtTile.y-innerTileOffset.y)}
-  
-    let resetOffset= 80;
 
-   
+    let rx = 1 - ((130 - (camera.position.x - 60)) / 420)
+    let rz = 1 - ((130 - (camera.position.z - 60)) / 420)
 
-   if (camera.position.x > resetOffset*2) {
-      
+    lookingAtTile = { x: Math.round(rx * 8), y: Math.round(rz * 8) };
+
+    rx = 1 - ((130 - (mouseOnGround.x - 60)) / 420)
+    rz = 1 - ((130 - (mouseOnGround.z - 60)) / 420)
+
+    pointingAtTile = { x: Math.ceil(rx * 8), y: Math.ceil(rz * 8) };
+
+    globalPosition = { x: startingPos.x + relOffset.x + (lookingAtTile.x - innerTileOffset.x), y: startingPos.y + relOffset.z + (lookingAtTile.y - innerTileOffset.y) }
+
+    let resetOffset = 80;
+
+    if (camera.position.x > resetOffset * 2) {
+
         controls.target.x -= LEAP
         camera.position.x -= LEAP
         carList.forEach((car) => (car.position.x -= LEAP))
-       relOffset.x +=4;
-      console.log("WRAPPED")
-       
-    } else if (camera.position.x < -resetOffset*2) {
+        relOffset.x += 4;
+        console.log("WRAPPED")
+
+    } else if (camera.position.x < -resetOffset * 2) {
         controls.target.x += LEAP
         camera.position.x += LEAP
         carList.forEach((car) => (car.position.x += LEAP))
-     relOffset.x -=4;
-     console.log("WRAPPED")
-     
+        relOffset.x -= 4;
+        console.log("WRAPPED")
+
     }
-    if (camera.position.z > resetOffset*2) {
+    if (camera.position.z > resetOffset * 2) {
         controls.target.z -= LEAP
         camera.position.z -= LEAP
         carList.forEach((car) => (car.position.z -= LEAP))
-        relOffset.z +=4;
+        relOffset.z += 4;
         console.log("WRAPPED")
-      
-    } else if (camera.position.z < -resetOffset*2) {
+
+    } else if (camera.position.z < -resetOffset * 2) {
         controls.target.z += LEAP
         camera.position.z += LEAP
         carList.forEach((car) => (car.position.z += LEAP))
-       relOffset.z -=4;
-       console.log("WRAPPED")
-    
+        relOffset.z -= 4;
+        console.log("WRAPPED")
+
     }
-   
+
 
     carList.forEach((car) => {
         car.r.set(
@@ -443,8 +414,8 @@ function render() {
             car.position.z += car.userData.z * car.speed
         }
     })
-    
-    _localTiles.forEach((lc)=>lc.render());
+
+    Object.values(_localTiles).forEach((lc) => lc.render());
 
     stats.end()
     renderer.render(scene, camera)
@@ -455,7 +426,7 @@ function render() {
 function loadCars({ x, z, cluster, direction }) {
     loader.load(`js/clusters/${cluster}.gltf`, (gltf) => {
         gltf.scene.traverse(function (child) {
-            if (child.isMesh) { 
+            if (child.isMesh) {
                 child.receiveShadow = true
                 child.castShadow = true
             }
@@ -474,18 +445,21 @@ function loadCars({ x, z, cluster, direction }) {
             )
 
         scene.add(gltf.scene)
+      
 
-        gltf.scene.children.forEach((e) => {
-            e.distance = 0
-            e.maxSpeed = 0.3
-            e.speed = e.maxSpeed
-            e.r = new THREE.Raycaster(
-                new THREE.Vector3(e.position.x, 2, e.position.z),
-                new THREE.Vector3(e.userData.x, 0, e.userData.z),
-                5,
-                15
-            )
-            carList.push(e)
-        })
+
+            gltf.scene.children.forEach((e) => {
+                e.distance = 0
+                e.maxSpeed = 0.3
+                e.speed = e.maxSpeed
+                e.r = new THREE.Raycaster(
+                    new THREE.Vector3(e.position.x, 2, e.position.z),
+                    new THREE.Vector3(e.userData.x, 0, e.userData.z),
+                    5,
+                    15
+                )
+                carList.push(e)
+            })
+       
     })
 }
